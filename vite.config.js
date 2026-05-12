424e09e13bd315309b683599477db5081b4a5283
DhruvHedaoo2704 <dhruv.hedaoo@gmail.com>
2026-05-12 12:44:10 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/To-Do-TaskFlow/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
